295ecd16918e46593d321a485b0e3385d76492d6
diegohdez12@gmail.com <diegohdez12@gmail.com>
2021-06-16 17:18:58 -0500

    fix: merge conflicts

--- MLKits/regressions/index.js
+++ MLKits/regressions/index.js
@@ -1,19 +1,24 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
+
 const loadCSV = require('./load-csv');
 const LinearRegression = require('./linear-regression');
 
-let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
-  shuffle: true,
-  splitTest: 50,
-  dataColumns: ['horsepower'],
-  labelColumns: ['mpg']
-});
+let { features,
+  labels,
+  testFeatures,
+  testLabels } = loadCSV('./cars.csv', {
+    shuffle: true,
+    splitTest: 50,
+    dataColumns: ['horsepower'],
+    labelColumns: ['mpg']
+  });
 
 const regression = new LinearRegression(features, labels, {
-  learningRate: 0.001,
-  iterations: 1
+  learningRation: 0.1,
+  iterations: 1,
 });
 
 regression.train();
-console.log('M value is', regression.m, 'B value is', regression.b);
+
+console.log('Updated M is:', regression.m, 'Updated B is:', regression.b);
