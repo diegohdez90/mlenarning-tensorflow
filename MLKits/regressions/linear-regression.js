a9ccbf6a56f359197eed008fe8fb34b282050366
diegohdez12@gmail.com <diegohdez12@gmail.com>
2021-06-16 13:35:36 -0500

    feat: add train method to compute through data with gradientDescent method

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -5,9 +5,15 @@ class LinearRegression {
     this.features = features;
     this.labels = labels;
     this.options = Object.assign({
-      learningRate: 0.1
+      learningRate: 0.1,
+      iterations: 1000
     }, options);
+  }
 
+  train() {
+    for (let i = 0; i < this.options.iterations; i++) {
+      this.gradientDescent();
+    }
   }
 }
 
