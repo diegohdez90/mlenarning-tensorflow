1db3b455045616d0734356430aeb3d2c10d99a79
diegohdez12@gmail.com <diegohdez12@gmail.com>
2021-06-16 14:15:24 -0500

    feat: add gradient descent method

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -8,6 +8,27 @@ class LinearRegression {
       learningRate: 0.1,
       iterations: 1000
     }, options);
+
+    this.m = 0;
+    this.b = 0;
+  }
+
+  gradientDescent() {
+    const currentGuessesForMPG = this.features.map(row => {
+      return this.m * row[0] + this.b
+    });
+
+    const bSlope = _.sum(currentGuessesForMPG.map((guess, index) => {
+      return guess - this.labels[index][0];
+    })) * 2 / this.features.legnth;
+
+    const mSlope = _.sum(currentGuessesForMPG.map((guess, index) => {
+      return -1 * this.features[index][0] * (this.labels[index][0] - guess);
+    })) * 2 / this.features.legnth;
+
+    this.m = this.m - mSlope * this.options.learningRate;
+    this.b = this.b - bSlope * this.options.learningRate;
+    
   }
 
   train() {
